221468d2b7e08c90a72ad93e69adae3a0c09e6b1
Ciarán Martin Conners <ciaran@filtered.ai>
2019-08-24 12:29:34 -0400

    First lessons.

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,37 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    loop {
+        println!("Please input your guess");
+
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+        
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Only numbers are valid as inputs.");
+                continue;
+            },
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("You guessed too low!"),
+            Ordering::Greater => println!("You guessed too high!"),
+            Ordering::Equal => {
+                println!("You guessed right! Congratulations!");
+                break;
+            },
+        }
+    }
+}
